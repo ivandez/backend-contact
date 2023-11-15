4379cc387dc0cfa254e72e7582ba78b69ad409a2
ivandez <ivan.mendez.dev@gmail.com>
2023-11-15 14:36:40 -0400

    implemented env vars

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,13 +1,14 @@
 import { DataSource } from "typeorm";
 import { Contact } from "./Contact";
+import "dotenv/config";
 
 const PostgresDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "password",
-  database: "postgres",
+  host: process.env.DB_HOST,
+  port: (process.env.DB_PORT as unknown as number) || 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [Contact],
   synchronize: true,
   logging: false,
